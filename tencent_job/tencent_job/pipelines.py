369d172ac37e970f6e676601a4690c1cd1e26c5c
pipoted <32660879+pipoted@users.noreply.github.com>
2019-04-03 02:28:14 +0800

    redis 实现

--- tencent_job/tencent_job/pipelines.py
+++ tencent_job/tencent_job/pipelines.py
@@ -4,24 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import pymongo
+import redis
 
 
 class TencentJobPipeline(object):
     def __init__(self):
-        self.conn = pymongo.MongoClient(host='localhost', port='27017')
-        self.db = self.conn['scrapy']
-        self.col = self.db['tencent_test']
+        self.conn = redis.Redis(password='xzx199110')
+        pass
 
     def process_item(self, item, spider):
-        data_dict = {
-            'nama': item['name'],
+        self.conn.set(item['name'], {
+            'name': item['name'],
             'types': item['types'],
             'nums': item['nums'],
             'city': item['city'],
-        }
-        self.col.insert_one(data_dict)
+        })
         return item
 
     def __del(self):
-        self.conn.close()
+        pass
