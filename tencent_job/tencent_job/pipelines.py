1b42f92eafe435cd114c49bad833bec0089ab232
pipoted <32660879+pipoted@users.noreply.github.com>
2019-04-03 01:59:54 +0800

    爬取腾讯招聘所有招聘信息并使用mysql进行存储

--- tencent_job/tencent_job/pipelines.py
+++ tencent_job/tencent_job/pipelines.py
@@ -4,8 +4,38 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
 
 
 class TencentJobPipeline(object):
+    def __init__(self):
+        self.conn = pymysql.Connect(
+            host='localhost',
+            user='root',
+            port=3306,
+            password='xzx199110',
+            database='scrapy',
+        )
+        self.cursor = self.conn.cursor()
+        sql = '''
+        create table if not exists tencent_test (
+        id int primary key not null auto_increment,
+        name varchar(255) not null,
+        types varchar(255) not null,
+        nums varchar(255) not null,
+        city varchar(255) not null
+        )
+        '''
+        self.cursor.execute(sql)
+        self.conn.commit()
+
     def process_item(self, item, spider):
+        sql = '''
+        insert into tencent_test (name, types, nums, city) values (%s, %s, %s, %s)
+        '''
+        self.cursor.execute(sql, (item['name'], item['types'], item['nums'], item['city']))
+        self.conn.commit()
         return item
+
+    def __del(self):
+        self.conn.close()
--- tencent_job/tencent_job/spiders/tencent_spider.py
+++ tencent_job/tencent_job/spiders/tencent_spider.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from tencent_job.items import TencentJobItem
 
 
 class TencentSpiderSpider(scrapy.Spider):
     name = 'tencent_spider'
     allowed_domains = ['job.tencent.com']
-    start_urls = ['http://job.tencent.com/']
+    start_urls = ['https://job.tencent.com/position.php?keywords=python&start={offset}#a'.format(offset=offset * 10) for offset in range(56)]
+    # start_urls = ['https://job.tencent.com/position.php?keywords=python&start=0#a']
 
     def parse(self, response):
-        pass
+        trs = response.xpath('//tr[@class="odd"]') + response.xpath('//tr[@class="even"]')
+        for tr in trs:
+            data_dict = TencentJobItem()
+            data_dict['name'] = tr.xpath('./td/a/text()').extract()[0]
+            data_dict['types'] = tr.xpath('./td[2]/text()').extract()[0]
+            data_dict['nums'] = tr.xpath('./td[3]/text()').extract()[0]
+            data_dict['city'] = tr.xpath('./td[4]/text()').extract()[0]
+
+            yield data_dict
