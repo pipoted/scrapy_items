4b33296609901178c919d63aec3033c54647a189
pipoted <32660879+pipoted@users.noreply.github.com>
2019-04-04 18:23:53 +0800

    登录成功

--- CSDN/CSDN/spiders/csdn_cookies.py
+++ CSDN/CSDN/spiders/csdn_cookies.py
@@ -5,7 +5,7 @@ import scrapy
 class CsdnCookiesSpider(scrapy.Spider):
     name = 'csdn_cookies'
     allowed_domains = ['www.csdn.net']
-    start_urls = ['https://edu.csdn.net/mycollege']
+    start_urls = ['https://my.csdn.net/my/mycsdn']
 
     cookies = {
         'uuid_tt_dd': '10_19293749450-1532423364703-346846',
@@ -15,20 +15,19 @@ class CsdnCookiesSpider(scrapy.Spider):
         'dc_session_id': '10_1544167850677.479857',
         'ARK_ID': 'JS75a9773184ed4c59a9049036f855ffc475a9',
         'UN': 'qq_36940418',
-        'ADHOC_MEMBERSHIP_CLIENT_ID1.0': '039131f8-e367-e1df-7000-620aa52c081b',
         'Hm_ct_6bcd52f51e9b3dce32bec4a3997715ac': '1788*1*PC_VC!5744*1*qq_36940418!6525*1*10_19293749450-1532423364703-346846',
         'c-login-auto': '19',
-        'TY_SESSION_ID': '41a81609-aa64-4764-b6f9-6c8a4238d952',
         'Hm_lvt_6bcd52f51e9b3dce32bec4a3997715ac': '1554300542,1554301261,1554310820,1554367625',
-        'SESSION': 'f09ccf38-7ff7-496b-9aa1-b233caa4ad8e',
-        'Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac': '1554369267',
-        'dc_tos': 'ppfj07',
         'UserName': 'qq_36940418',
-        'UserInfo': '4650fe97893343d4833b5ef5d530c727',
-        'UserToken': '4650fe97893343d4833b5ef5d530c727',
+        'UserInfo': '227f138c4964426482c2837ff558fab0',
+        'UserToken': '227f138c4964426482c2837ff558fab0',
         'UserNick': 'qq_36940418',
         'AU': '31C',
-        'BT': '1554369299202',
+        'BT': '1554372993791',
+        'SESSION': '7083a889-e509-410a-9210-c3ccfab490d2',
+        'ci_session': 'a%3A6%3A%7Bs%3A10%3A%22session_id%22%3Bs%3A32%3A%22fd18a42924aca7fd6bd0a8f772d4cfee%22%3Bs%3A10%3A%22ip_address%22%3Bs%3A13%3A%22106.39.149.35%22%3Bs%3A10%3A%22user_agent%22%3Bs%3A120%3A%22Mozilla%2F5.0+%28Macintosh%3B+Intel+Mac+OS+X+10_14_3%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F72.0.3626.121+Safari%2F537.3%22%3Bs%3A13%3A%22last_activity%22%3Bi%3A1554373069%3Bs%3A9%3A%22user_data%22%3Bs%3A0%3A%22%22%3Bs%3A8%3A%22userInfo%22%3Bs%3A11%3A%22qq_36940418%22%3B%7Da77b916cbc390aa8eb0291835fbd39f7',
+        'Hm_lpvt_6bcd52f51e9b3dce32bec4a3997715ac': '1554373070',
+        'dc_tos': 'ppflxq',
     }
 
     def start_requests(self):
