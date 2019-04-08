2a95778ce87112cb0f94b9111572b7347d1bc6e6
pipoted <32660879+pipoted@users.noreply.github.com>
2019-04-08 13:11:45 +0800

    登录失败

---
+++ taobao/start.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*- coding: <utf-8> -*-
+__author__ = 'xiao'
+__date__ = '2019/4/8 12:48 PM'
+
+from scrapy import cmdline
+
+cmdline.execute(argv=['scrapy', 'crawl', 'taobao_login'])
